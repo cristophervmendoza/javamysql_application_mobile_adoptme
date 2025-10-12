691d37be7f39e4b1ba558a52f1ef8f4f22748533
Cristopher MV <cristopher.v.mendoza@gmail.com>
2025-10-12 12:01:01 -0500

    Add splash screen and authentication activities
    Introduces splash screen animations and logo to MainActivity, transitioning to a new StartUpActivity. Adds LoginActivity and StartUpActivity with corresponding layouts and manifest entries. Updates activity_main.xml to include a centered logo and sets background color.

--- app/src/main/java/com/example/javamysql_application_mobile_adoptme/MainActivity.java
+++ app/src/main/java/com/example/javamysql_application_mobile_adoptme/MainActivity.java
@@ -1,12 +1,19 @@
-    package com.example.javamysql_application_mobile_adoptme;
+package com.example.javamysql_application_mobile_adoptme;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.example.javamysql_application_mobile_adoptme.Auth.ui.startup.StartUpActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,47 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        final ImageView logo = findViewById(R.id.img_center_logo);
+
+        Animation splashAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_animation);
+        final Animation exitAnimation = AnimationUtils.loadAnimation(this, R.anim.splash_exit_animation);
+
+        splashAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                new Handler().postDelayed(() -> {
+                    logo.startAnimation(exitAnimation);
+                }, 500);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        exitAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {}
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                Intent intent = new Intent(MainActivity.this, StartUpActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {}
+        });
+
+
+        logo.startAnimation(splashAnimation);
+
     }
 }
